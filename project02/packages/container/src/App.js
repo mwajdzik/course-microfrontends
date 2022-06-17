25f5d56a45f16fd3109da1a42acfe9c7c1bb239b
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2022-06-17 19:17:51 +0200

    Microfrontends Add marketing module

--- project02/packages/container/src/App.js
+++ project02/packages/container/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import MarketingApp from "./components/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
         <h1>My App !!!</h1>
         <hr/>
-        {/*<MarketingApp/>*/}
+        <MarketingApp/>
     </div>;
 };
