38a8557c9c08c34b5c92dbdb229e5c8ffd77ffd1
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2022-06-17 19:07:16 +0200

    Microfrontends Add invalidation of index.html

--- project02/packages/container/src/App.js
+++ project02/packages/container/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default () => {
     return <div>
-        <h1>My App!!!</h1>
+        <h1>My App !!!</h1>
         <hr/>
         {/*<MarketingApp/>*/}
     </div>;
