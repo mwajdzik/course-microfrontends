3c4592647a53c7359c1e6799b4e5fd50a2aaf2d8
Wajdzik, Maciej <wajdzik.maciej@gmail.com>
2022-06-20 18:46:16 +0200

    Microfrontends Lazy loading

--- project02/packages/container/src/App.js
+++ project02/packages/container/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
-import AuthApp from "./components/AuthApp";
-import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
+
+// import AuthApp from "./components/AuthApp";
+// import MarketingApp from "./components/MarketingApp";
+
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
@@ -17,10 +22,12 @@ export default () => {
                 <div>
                     <Header/>
 
-                    <Switch>
-                        <Route path="/auth" component={AuthApp}/>
-                        <Route path="/" component={MarketingApp}/>
-                    </Switch>
+                    <Suspense fallback={<Progress/>}>
+                        <Switch>
+                            <Route path="/auth" component={AuthLazy}/>
+                            <Route path="/" component={MarketingLazy}/>
+                        </Switch>
+                    </Suspense>
                 </div>
             </BrowserRouter>
         </StylesProvider>
